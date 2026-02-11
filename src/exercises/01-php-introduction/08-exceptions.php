f69b266f24df487f6a234f58a102223b778d2c37
Jack <n00256110@iadt.ie>
2026-02-11 00:03:13 +0000

    Exceptions exercises finished

--- src/exercises/01-php-introduction/08-exceptions.php
+++ src/exercises/01-php-introduction/08-exceptions.php
@@ -35,6 +35,22 @@
             }
             return sqrt($input);
         }
+
+        $num1 = 16;
+        $num2 = 25;
+        $num3 = -9;
+
+        try{
+        echo "The square root of $num1 is " . calculateSquareRoot($num1) . ". <br>";
+        echo "The square root of $num2 is " . calculateSquareRoot($num2) . ". <br>";
+        echo "The square root of $num3 is " . calculateSquareRoot($num3) . ". <br>";
+        echo "This line does not execute as it comes after an error.";
+        }
+
+        catch (Exception $e) {
+            echo "Error: " . $e->getMessage();
+        }
+        
         ?>
     </div>
 
@@ -52,6 +68,49 @@
     <div class="output">
         <?php
         // TODO: Write your solution here
+
+        function validateEmail($email) {
+            $atSymbolCount = 0;
+            for ($i = 0; $i < strlen($email); $i++) {
+                if ($email[$i] === "@") {
+                    $atSymbolCount++;
+                }
+            }
+            if ($atSymbolCount == 0) {
+                throw new Exception("No @ symbol to indicate valid email.<br>");
+            }
+            echo "This is a valid email. <br>";
+        }
+
+        $email1 = "user@example.com";
+        $email2 = "invalid-email";
+        $email3 = "test@test.ie";
+
+        try{
+        echo "testing email: $email1<br>";
+        validateEmail($email1);
+        }
+        catch (Exception $e) {
+            echo "Error: " . $e->getMessage();
+        }
+
+        try{
+        echo "testing email: $email2<br>";
+        validateEmail($email2);
+        }
+        catch (Exception $e) {
+            echo "Error: " . $e->getMessage();
+        }
+
+        try {
+        echo "testing email: $email3<br>";
+        validateEmail($email3);
+        }
+        catch (Exception $e) {
+            echo "Error: " . $e->getMessage();
+        }
+
+
         ?>
     </div>
 
@@ -68,6 +127,45 @@
     <div class="output">
         <?php
         // TODO: Write your solution here
+
+        function processFile($filename){
+            if ($filename == "") {
+                throw new Exception("Filename must not be empty. <br> <br>");
+            }
+            return $filename;
+        }
+
+        $file1 = ['name'=> 'dog.jpeg'];
+        $file2 = ['name'=> ''];
+        $file3 = ['name'=> 'helowordl.txt'];
+
+        try {
+            echo "Processing file 1 <br>";
+            echo "File name: " . processFile($file1['name']) . "<br> <br>";
+        }
+        catch (Exception $e) {
+            echo "Error: " . $e->getMessage();
+        }
+
+        try {
+            echo "Processing file 2 <br>";
+            echo "File name: " . processFile($file2['name']) . "<br> <br>";
+        }
+        catch (Exception $e) {
+            echo "Error: " . $e->getMessage();
+        }
+
+        try {
+            echo "Processing file 3 <br>";
+            echo "File name: " . processFile($file3['name']) . "<br> <br>";
+        }
+        catch (Exception $e) {
+            echo "Error: " . $e->getMessage();
+        }
+
+        finally {
+            echo "Processing complete.";
+        }
         ?>
     </div>
 
